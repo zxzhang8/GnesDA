ce4af8e65704620be375a0236b3cc77129d02333
Qianyuwww <126842461+Qianyuwww@users.noreply.github.com>
2025-01-06 16:21:10 +0800

    Create start_protein.sh

---
+++ start_protein.sh
@@ -0,0 +1,14 @@
+dataset_list=(uniprot uniref)
+dist_type_list=(ed nw)
+seed_list=(666 555 444)
+for dataset in ${dataset_list[@]}; do
+    for seed in ${seed_list[@]}; do
+        for dist_type in ${dist_type_list[@]}; do
+            train_flag=${dataset}_${dist_type}_${seed}
+            echo ${train_flag}
+            nohup python train.py --data_type protein --dist_type ${dist_type} --shuffle-seed ${seed} --epochs 300 --conv_layers 5 --nt 1000 > train_log/${train_flag} &
+            PID0=$!
+            wait $PID0
+        done
+    done
+done
